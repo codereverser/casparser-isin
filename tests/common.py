af2872a017ec0e592ab90904c44552295d6939fa
Sandeep Somasekharan <codereverser@gmail.com>
2021-02-12 23:54:45 +0530

    test cases for cli

---
+++ tests/common.py
@@ -0,0 +1,101 @@
+import argparse
+import builtins
+from http.client import HTTPResponse
+from io import BytesIO, StringIO
+import os
+import socket
+from urllib import error, request
+
+import pytest
+
+from casparser_isin import cli
+
+
+class MockResponse(HTTPResponse):
+    def __init__(
+        self, mock_request: request.Request, remote_db_version="2000.01.01", fail_on_urls=None
+    ):
+        self.__mock_request = mock_request
+        self.__remote_db_version = remote_db_version
+        if not isinstance(fail_on_urls, list):
+            fail_on_urls = [fail_on_urls]
+        self.__mock_fail_on_urls = fail_on_urls
+        sock = socket.socket()
+        super().__init__(sock)
+
+    def read(self, amt=None):
+        if self.__mock_request.full_url in self.__mock_fail_on_urls:
+            raise error.HTTPError(self.__mock_request.full_url, 400, "Mock HTTP Error", {}, fp=None)
+        elif self.__mock_request.full_url == cli.META_URL:
+            return f"version={self.__remote_db_version}\ndbformat=1".encode()
+        elif self.__mock_request.full_url == cli.DB_URL:
+            return b"mock_data"
+
+
+class MockFileManager:
+    def __init__(self):
+        self.files = {}
+        self._open = builtins.open
+
+    def open(self, name, mode="r", buffering=-1, **options):
+        print(name, mode, buffering, options)
+        name = os.path.abspath(name)
+        if mode.startswith("r") and name not in self.files:
+            # We have to let some files through
+            return self._open(name, mode, buffering, **options)
+            # This causes stracktraces not to display
+            # raise IOError(2, "No such file or directory: '%s'" % name)
+
+        if mode.startswith("w") or (mode.startswith("a") and name not in self.files):
+            if "b" in mode:
+                buf = BytesIO()
+            else:
+                buf = StringIO()
+                buf.close = lambda: None
+            self.files[name] = buf
+
+        buf = self.files[name]
+
+        if mode.startswith("r"):
+            buf.seek(0)
+        elif mode.startswith("a"):
+            buf.seek(0)
+        return buf
+
+    def write(self, name, text):
+        name = os.path.abspath(name)
+        buf = StringIO(text)
+        buf.close = lambda: None
+        self.files[name] = buf
+
+    def read(self, name):
+        name = os.path.abspath(name)
+        if name not in self.files:
+            raise IOError(2, "No such file or directory: '%s'" % name)
+
+        return self.files[name].getvalue()
+
+
+@pytest.fixture
+def mockopen(monkeypatch):
+    manager = MockFileManager()
+    monkeypatch.setattr(builtins, "open", manager.open)
+    return manager
+
+
+@pytest.fixture
+def version_cli(monkeypatch):
+    monkeypatch.setattr(
+        argparse.ArgumentParser,
+        "parse_args",
+        lambda x: argparse.Namespace(update=False, version=True),
+    )
+
+
+@pytest.fixture
+def update_cli(monkeypatch):
+    monkeypatch.setattr(
+        argparse.ArgumentParser,
+        "parse_args",
+        lambda x: argparse.Namespace(update=True, version=False),
+    )
---
+++ tests/test_cli.py
@@ -0,0 +1,71 @@
+import functools
+import logging
+from urllib import request
+
+from casparser_isin import cli
+
+from .common import MockResponse, mockopen, update_cli, version_cli
+
+
+class TestCLI:
+    """Common test cases for all available parsers."""
+
+    def test_version(self, capsys, version_cli):
+
+        metadata = cli.get_metadata()
+        expected_output = (
+            f"cli-version : {metadata['cli-version']}\n"
+            f"db-version  : {metadata['version']}\n"
+            f"db-format   : {metadata['dbformat']}\n"
+        )
+
+        cli.main()
+        captured = capsys.readouterr()
+        assert captured.err == ""
+        assert captured.out == expected_output
+
+    def test_meta_fail(self, monkeypatch, update_cli, caplog, mockopen):
+        def mock_urlopen(request_obj, db_version="3099.01.01", fail_on_urls=None):
+            return MockResponse(
+                request_obj, remote_db_version=db_version, fail_on_urls=fail_on_urls
+            )
+
+        caplog.set_level(logging.INFO)
+
+        with monkeypatch.context() as m:
+            m.setattr(
+                request, "urlopen", functools.partial(mock_urlopen, fail_on_urls=cli.META_URL)
+            )
+            cli.main()
+        expected_log_msgs = [
+            "Fetching remote isin db metadata",
+            "Received error from remote server :: Mock HTTP Error",
+        ]
+        assert [x.message for x in caplog.records] == expected_log_msgs
+
+        with monkeypatch.context() as m:
+            m.setattr(request, "urlopen", functools.partial(mock_urlopen, fail_on_urls=cli.DB_URL))
+            cli.main()
+            caplog_records = [x.message for x in caplog.records][-2:]
+            assert caplog_records == [
+                "Fetching database version :: 3099.1.1",
+                "Error fetching isin database :: Mock HTTP Error",
+            ]
+
+    def test_update_success(self, monkeypatch, update_cli, caplog, mockopen):
+        def mock_urlopen(request_obj, db_version="3099.01.01", fail_on_urls=None):
+            return MockResponse(
+                request_obj, remote_db_version=db_version, fail_on_urls=fail_on_urls
+            )
+
+        caplog.set_level(logging.INFO)
+
+        with monkeypatch.context() as m:
+            m.setattr(request, "urlopen", functools.partial(mock_urlopen, db_version="1990.01.01"))
+            cli.main()
+        assert caplog.records[-1].message == "casparser-isin database is already upto date"
+
+        with monkeypatch.context() as m:
+            m.setattr(request, "urlopen", mock_urlopen)
+            cli.main()
+        assert caplog.records[-1].message == "Updated casparser-isin database."
